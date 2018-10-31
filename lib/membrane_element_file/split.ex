012bee21fd5d3cf92033f0bb7fd2d5664035532a
Mateusz Front <mateusz.front@swmansion.com>
2018-10-31 16:37:51 +0100

    SplitEvent file name fix

--- lib/membrane_element_file/split.ex
+++
@@ -1,9 +0,0 @@
-defmodule Membrane.Element.File.SplitEvent do
-  @moduledoc """
-  Default event that closes current and opens new file in
-  `Membrane.Element.File.Sink.Multi`.
-  """
-  @derive Membrane.EventProtocol
-
-  defstruct []
-end
