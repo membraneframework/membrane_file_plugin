b099ff224bb7ab8cc20b594cba6e4f481b1267f3
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2022-11-18 10:56:52 +0100

    Rewrite on core 0.11

--- examples/sink_multi.exs
+++ examples/sink_multi.exs
@@ -10,13 +10,13 @@ defmodule Splitter do
   alias Membrane.Buffer
   alias Membrane.File.SplitEvent
 
-  def_input_pad :input, demand_unit: :bytes, demand_mode: :auto, caps: Membrane.RemoteStream
-  def_output_pad :output, demand_mode: :auto, caps: Membrane.RemoteStream
+  def_input_pad :input, demand_unit: :bytes, demand_mode: :auto, accepted_format: Membrane.RemoteStream
+  def_output_pad :output, demand_mode: :auto, accepted_format: Membrane.RemoteStream
 
   def_options head_size: [type: :integer]
 
-  def handle_init(opts) do
-    {:ok, opts |> Map.from_struct() |> Map.put(:split?, true)}
+  def handle_init(_ctx, opts) do
+    {[], opts |> Map.from_struct() |> Map.put(:split?, true)}
   end
 
   @impl true
@@ -29,11 +29,11 @@ defmodule Splitter do
       buffer: {:output, %Buffer{payload: tail}}
     ]
 
-    {{:ok, actions}, %{split?: false}}
+    { actions, %{split?: false}}
   end
 
   def handle_process(:input, buffer, _ctx, %{split?: false}) do
-    {{:ok, buffer: {:output, buffer}}, %{split?: false}}
+    {[buffer: {:output, buffer}], %{split?: false}}
   end
 end
 
@@ -44,30 +44,28 @@ defmodule SinkMultiExamplePipeline do
 
   @doc false
   @impl true
-  def handle_init(target) do
-    links =
-      [
-        file_source: %Membrane.File.Source{location: "input.bin"},
-        filter: %Splitter{head_size: 10},
-        file_sink: %Membrane.File.Sink.Multi{location: "/tmp/output", extension: ".bin"}
-      ]
-      |> ParentSpec.link_linear()
+  def handle_init(_ctx, target) do
+    structure = [
+      child(:file_source, %Membrane.File.Source{location: "input.bin"})
+      |> child(:filter, %Splitter{head_size: 10})
+      |> child(:file_sink, %Membrane.File.Sink.Multi{location: "/tmp/output", extension: ".bin"})
+    ]
 
-    {{:ok, spec: %ParentSpec{links: links}, playback: :playing}, %{target: target}}
+    {[spec: structure, playback: :playing], %{target: target}}
   end
 
   @impl true
   def handle_element_end_of_stream({:file_sink, :input}, _ctx, state) do
     send(state.target, :done)
-    {:ok, state}
+    {[], state}
   end
 
   def handle_element_end_of_stream(_other, _ctx, state) do
-    {:ok, state}
+    {[], state}
   end
 end
 
-{:ok, pid} = SinkMultiExamplePipeline.start_link(self())
+{:ok, _supervisor_pid, pid} = SinkMultiExamplePipeline.start_link(self())
 
 receive do
   :done -> SinkMultiExamplePipeline.terminate(pid)
