46ee1cf20ca015c8689c19ac37fe23cd64700395
Feliks Pobiedziński <38541925+FelonEkonom@users.noreply.github.com>
2022-11-22 11:45:25 +0100

    Update version to 0.13.1 (#34)

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.File.Plugin.Mixfile do
   use Mix.Project
 
-  @version "0.13.0"
+  @version "0.13.1"
 
   @github_url "https://github.com/membraneframework/membrane_file_plugin"
 
