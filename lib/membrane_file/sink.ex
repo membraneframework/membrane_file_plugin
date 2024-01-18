3254c01b8f96d5e891435c7afdc9a412f7628020
kidp330 <j.migdal42@gmail.com>
2024-01-18 15:08:24 +0000

    Update docs as per code review

--- lib/membrane_file/sink.ex
+++ lib/membrane_file/sink.ex
@@ -4,14 +4,16 @@ defmodule Membrane.File.Sink do
   Can also be used as a pipe to standard output by setting location to :stdout,
   though this requires additional configuration.
 
-  Pipeline logs are directed to standard output by default. To separate them from the sink's output
-  we reccomend redirecting the logger to standard error. See 'examples/file_to_pipe.exs'
-
   When `Membrane.File.SeekSinkEvent` is received, the element starts writing buffers starting
   from `position`. By default, it overwrites previously stored bytes. You can set `insert?`
   field of the event to `true` to start inserting new buffers without overwriting previous ones.
   Please note, that inserting requires rewriting the file, what negatively impacts performance.
   For more information refer to `Membrane.File.SeekSinkEvent` moduledoc.
+
+  Pipeline logs are directed to standard output by default. To separate them from the sink's output
+  we recommend redirecting the logger to standard error. For simple use cases using the default logger
+  configuration (like stand-alone scripts) this can be achieved by simply calling redirect_logs/1.
+  See examples/file_to_pipe.exs for a working example.
   """
   use Membrane.Sink
 
