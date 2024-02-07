78a4e44157c30326982e3248c5e627f39a9659ef
kidp330 <j.migdal42@gmail.com>
2024-02-07 11:31:48 +0100

    Bump to 0.17.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.File.Plugin.Mixfile do
   use Mix.Project
 
-  @version "0.16.0"
+  @version "0.17.0"
 
   @github_url "https://github.com/membraneframework/membrane_file_plugin"
 
